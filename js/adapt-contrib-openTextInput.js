8ed03bc758420f440268e29ad14706e9a5973d41
Eleanor Heath <eleanor.heath@learningpoool.com>
2020-05-28 13:14:19 +0100

    Striped out less styles and converted JS to use ES6 classes

--- js/adapt-contrib-openTextInput.js
+++ js/adapt-contrib-openTextInput.js
@@ -12,22 +12,24 @@ define([
   'core/js/enums/buttonStateEnum'
 ], function(Adapt, QuestionView, BUTTON_STATE) {
 
-  var OpenTextInput = QuestionView.extend({
+  class OpenTextInput extends QuestionView {
 
-    events: {
-      'keyup .opentextinput__item-textbox': 'onKeyUpTextarea'
-    },
+    events() {
+        return {
+        'keyup .opentextinput__item-textbox': 'onKeyUpTextarea'
+      }
+    }
 
-    formatPlaceholder: function() {
+    formatPlaceholder() {
       // Replace quote marks in placholder.
       var placeholder = this.model.get('placeholder') || '';
       
       placeholder = placeholder.replace(/"/g, "'");
 
       this.model.set('placeholder', placeholder);
-    },
+    }
 
-    setupQuestion: function() {
+    setupQuestion() {
       this.listenTo(this.model, 'change:_isComplete', this.onCompleteChanged);
 
       // Open Text Input cannot show feedback, but may have been set in older courses
@@ -71,15 +73,15 @@ define([
       }
 
       this.model.set('_buttons', buttons);
-    },
+    }
 
-    onCompleteChanged: function(model, isComplete, buttonState) {
+    onCompleteChanged(model, isComplete, buttonState) {
       this.$textbox.prop('disabled', isComplete);
 
       if (isComplete) {
         if (model.get('_canShowModelAnswer')) {
           // Keep the action button enabled so we can show the model answer.
-          this.$('.buttons-action').a11y_cntrl_enabled(true);
+          this.$('.btn__action').a11y_cntrl_enabled(true);
 
           if (!_.isEmpty(buttonState)) {
             // Toggle the button.
@@ -91,9 +93,9 @@ define([
           }
         }
       }
-    },
+    }
 
-    canSubmit: function() {
+    canSubmit() {
       var answer = this.model.get('_userAnswer');
 
       if (typeof String.prototype.trim !== 'function') {
@@ -103,13 +105,13 @@ define([
       }
 
       return answer && answer.trim() !== '';
-    },
+    }
 
-    isCorrect: function() {
+    isCorrect() {
       return this.canSubmit();
-    },
+    }
 
-    onQuestionRendered: function() {
+    onQuestionRendered() {
       this.listenTo(this.buttonsView, 'buttons:stateUpdate', this.onActionClicked);
 
       if (this.$textbox === undefined) {
@@ -131,12 +133,12 @@ define([
         // Model answer has been disabled.
         // Force setting the correct/submitted state.
         this.model.set('_buttonState', BUTTON_STATE.CORRECT);
-        this.$('.buttons-action').a11y_cntrl_enabled(false);
+        this.$('.btn__action').a11y_cntrl_enabled(false);
         this.$textbox.prop('disabled', true);
       }
-    },
+    }
 
-    getUserAnswer: function() {
+    getUserAnswer() {
       var identifier = this.model.get('_id') + '-OpenTextInput-UserAnswer';
       var userAnswer = '';
 
@@ -148,18 +150,18 @@ define([
       }
 
       return false;
-    },
+    }
 
-    supportsHtml5Storage: function() {
+    supportsHtml5Storage() {
       // check for html5 local storage support
       try {
         return 'localStorage' in window && typeof window['localStorage'] !== 'undefined';
       } catch (e) {
         return false;
       }
-    },
+    }
 
-    countCharacters: function() {
+    countCharacters() {
       var charLengthOfTextarea = this.$textbox.val().length;
       var allowedCharacters = this.model.get('_allowedCharacters');
       if (allowedCharacters != null) {
@@ -168,35 +170,36 @@ define([
       } else {
         this.$('.opentextinput__count-amount').html(charLengthOfTextarea);
       }
-    },
+    }
 
-    onKeyUpTextarea: _.throttle(function() {
-      this.limitCharacters();
-      var text = this.$textbox.val();
-      this.model.set('_userAnswer', text);
+    onKeyUpTextarea() {
+      _.throttle(function() {
+        this.limitCharacters();
+        var text = this.$textbox.val();
+        this.model.set('_userAnswer', text);
 
-      this.countCharacters();
+        this.countCharacters();
 
-      if (this.saveTimeout) {
-        clearTimeout(this.saveTimeout);
-      }
+        if (this.saveTimeout) {
+          clearTimeout(this.saveTimeout);
+        }
 
-      var self = this;
-      this.saveTimeout = setTimeout(function() {
-        self.storeUserAnswer();
-      }, 2000);
+        var self = this;
+        this.saveTimeout = setTimeout(function() {
+          self.storeUserAnswer();
+        }, 2000);
+      }, 300);
+    }
 
-    }, 300),
-
-    limitCharacters: function() {
+    limitCharacters() {
       var allowedCharacters = this.model.get('_allowedCharacters');
       if (allowedCharacters != null && this.$textbox.val().length > allowedCharacters) {
         var substringValue = this.$textbox.val().substring(0, allowedCharacters);
         this.$textbox.val(substringValue);
       }
-    },
+    }
 
-    storeUserAnswer: function() {
+    storeUserAnswer() {
       // Use unique identifier to avoid collisions with other components
       var identifier = this.model.get('_id') + '-OpenTextInput-UserAnswer';
 
@@ -214,22 +217,15 @@ define([
 
       this.$autosave.css({opacity: 100});
       this.$autosave.delay(1000).animate({opacity: 0});
-    },
+    }
 
-    onActionClicked: function(buttonState) {
+    onActionClicked(buttonState) {
       if (this.model.get('_isComplete')) {
         this.onCompleteChanged(this.model, true, buttonState);
       }
-    },
+    }
 
-    updateActionButton: function(buttonText) {
-      // Keep the action button enabled so we can show the model answer
-      this.$('.buttons-action').a11y_cntrl_enabled(true);
-
-      this.$('.opentextinput__action-button').html(buttonText);
-    },
-
-    postRender: function() {
+    postRender() {
       if (this.$('.opentextinput__item-modelanswer').height() <= 0) {
         this.$('.opentextinput__item-textbox, .opentextinput__count-characters').css('height', 'auto');
       } else {
@@ -242,23 +238,21 @@ define([
       this.$('.opentextinput__item-modelanswer').addClass('opentextinput__hide-modelanswer');
 
       QuestionView.prototype.postRender.call(this);
-    },
+    }
 
-    showCorrectAnswer: function() {
+    showCorrectAnswer() {
       this.model.set('_buttonState', BUTTON_STATE.HIDE_CORRECT_ANSWER);
-      this.updateActionButton(this.model.get('_buttons').showUserAnswer);
-
+      
       this.$textbox.hide();
       this.$countChars.hide();
       this.$modelAnswer.addClass('opentextinput__show-modelanswer').removeClass('opentextinput__hide-modelanswer');
 
       this.scrollToTextArea();
-    },
+    }
 
-    hideCorrectAnswer: function() {
+    hideCorrectAnswer() {
       this.model.set('_buttonState', BUTTON_STATE.SHOW_CORRECT_ANSWER);
-      this.updateActionButton(this.model.get('_buttons').showModelAnswer);
-
+      
       if (this.$textbox === undefined) {
         this.$textbox = this.$('textarea.opentextinput__item-textbox');
       }
@@ -275,51 +269,49 @@ define([
 
       this.$countChars.show();
       this.$modelAnswer.addClass('opentextinput__hide-modelanswer').removeClass('opentextinput__show-modelanswer');
-    },
+    }
 
-    scrollToTextArea: function() {
+    scrollToTextArea() {
       // Smooth scroll to top of TextArea
       Adapt.scrollTo(this.$('.opentextinput__widget'), {
         duration: 400,
         offset: -parseInt($('#wrapper').css('padding-top'))
       });
-    },
+    }
 
     /**
      * Used by adapt-contrib-spoor to get the user's answers in the format required by the cmi.interactions.n.student_response data field
      */
-    getResponse: function() {
-      var userAnswer = this.model.get('_userAnswer') || '';
-
-      return userAnswer;
-    },
+    getResponse() {
+      return this.model.get('_userAnswer') || '';
+    }
 
     /**
      * Used by adapt-contrib-spoor to get the type of this question in the format required by the cmi.interactions.n.type data field
      */
-    getResponseType: function() {
+    getResponseType() {
       return 'fill-in';
-    },
+    }
 
-    getInteractionObject: function() {
+    getInteractionObject() {
       return {
         correctResponsesPattern: [
           this.model.get('modelAnswer')
         ]
       };
-    },
+    }
 
     /**
      * Used by questionView. Clears the models on Revisit userAnswer so input appears blank
      */
-    resetQuestionOnRevisit: function() {
+    resetQuestionOnRevisit() {
       this.resetQuestion();
-    },
+    }
 
     /**
      * Used by questionView. Clears the models userAnswer onResetClicked so input appears blank
      */
-    resetQuestion: function() {
+    resetQuestion() {
       this.model.set('_userAnswer', '');
 
       if (this.$textbox === undefined) {
@@ -328,7 +320,7 @@ define([
 
       this.$textbox.val(this.model.get('_userAnswer'));
     }
-  });
+  };
 
   Adapt.register('openTextInput', OpenTextInput);
   
