e432a14dae5f37262c4cc836032aa22a18d9288b
Cahir O'Doherty <41006337+cahirodoherty-learningpool@users.noreply.github.com>
2021-05-18 09:40:22 +0100

    Cahir.further changes (#61)
    * Tidy ups and changes
    
    * Correcting model answer toggle

--- js/adapt-contrib-openTextInput.js
+++ js/adapt-contrib-openTextInput.js
@@ -23,25 +23,22 @@ define([
    * Spoor cannot store text input values and so the completion status of this component does not get
    * saved or restored properly. This function ensures that the question's completion status is
    * restored in a way that other extensions e.g. learning objectives can obtain accurate data for processing
-   *
    */
   function restoreQuestionStatusPolyfill() {
     Adapt.components.each(function(component) {
-      if (component.get('_component') !== 'openTextInput') {
-        return;
-      }
+      if (component.get('_component') !== 'openTextInput') return;
+
+      if (!component.get('_isComplete')) return;
 
       // If the component is complete then it must be correct
       // _isInteractionComplete needs to be set to true so marking is restored correctly
-      if (component.get('_isComplete')) {
-        component.set({
-          _isCorrect: true,
-          _isInteractionComplete: true
-        });
+      component.set({
+        _isCorrect: true,
+        _isInteractionComplete: true
+      });
 
-        // Add a manual trigger just in case any extensions listening for this change have already loaded
-        component.trigger('change:_isComplete', component, true);
-      }
+      // Add a manual trigger just in case any extensions listening for this change have already loaded
+      component.trigger('change:_isComplete', component, true);
     });
   }
 
